d7919b6f8632a1e3f9d2077bc39048be316ae226
Juan Manuel Aguero <juan@impactinstitute.com>
2023-02-20 10:27:55 +0100

    Error when requesting not allowed url

--- Program.fs
+++ Program.fs
@@ -25,10 +25,11 @@ let langHandler =
         let getSec = configuration.GetSection("Lang")
         // match query, prob there's a more neat way to implement this
         let handler =
+            printfn "%A" ctx.GetEndpoint
             match ctx.TryGetQueryStringValue "lang" with
             | Some a when a.Length < 1 -> RequestErrors.badRequest (json {Message = "Lang can't be an empty string"})
             | Some l when l.Length > 2 -> RequestErrors.badRequest (json { Error = $"{l} is {l.Length} length and lang param only accepts ISO 639-1 codes"})
-            | Some "en" -> json { Message = getSec.GetValue("en:msg")} // It is possible to add a type between <>: getSec.GetValue<string>("en:msg")}
+            | Some "en" -> text $"{ctx.GetEndpoint}" // It is possible to add a type between <>: getSec.GetValue<string>("en:msg")}
             | Some "de" -> json { Message = getSec.GetValue<string>("de:msg")}
             | Some "es" -> json { Message = getSec.GetValue<string>("es:msg")}
             | Some unknownLanguage ->  
@@ -38,11 +39,11 @@ let langHandler =
             | None -> RequestErrors.badRequest (json {Message = "Language can't be empty"})        
         handler next ctx
 
+
 let myWebApp =
     choose [
-        route "/" >=> langHandler
-        route "/json"   >=> json Message
-        route "/text"   >=> text "Hello World!"
+        route "/api/greet" >=> langHandler
+        RequestErrors.NOT_FOUND "Not Found" 
     ]
 
 
